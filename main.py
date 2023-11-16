b2afc59e29f322b5496286ec525217d82de8c3bc
aleksandrlytkin <Dracca@mail.ru>
2023-11-16 18:41:42 +0300

    исправил текст

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
- # This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a = 1.5
+b = 1.5
+c = 2.5
+qaz = a + b + c
+if qaz >= 2:
+    print(qaz)
